2f734c0d5a55d8e1b3d62aa39bad0e8f850b9b77
redder97 <red9gb@gmail.com>
2023-03-03 00:18:24 +0800

    created layer between service and repository. changes to tsconfig.json (allow null/undefined)

--- src/service/auth/auth-service.ts
+++ src/service/auth/auth-service.ts
@@ -2,26 +2,23 @@ import { PrismaClient } from '@prisma/client';
 import { LoginRequest } from '../../api/rest/auth/definition';
 import auth from '../../util/auth';
 import { UserView } from './definition';
-
-const prisma = new PrismaClient();
+import userRepository from '../../repository/user-repository';
 
 const login = async (login: LoginRequest) => {
   if (!login) {
     throw Error('login request not found.');
   }
 
+  const { username, password } = login;
+
   try {
-    const foundUser = await prisma.user.findFirstOrThrow({
-      where: {
-        username: login.username,
-      },
-    });
+    const foundUser = await userRepository.findByUsername(username);
 
     if (!foundUser) {
       throw Error('user not found');
     }
 
-    if (foundUser.password && !auth.comparePassword(login.password, foundUser.password)) {
+    if (foundUser.password && !auth.comparePassword(password, foundUser.password)) {
       throw Error('invalid password');
     }
 
